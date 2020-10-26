b8e52b9926e4b455cc9bfe698600cefd5fb089a9
Navneet <navneetjh5@gmail.com>
2020-10-26 20:02:01 +0530

    ADDED: Test file to check Loading

---
+++ src/test/java/com/cg/censusanalyser/StateAnalyserCensusTest.java
@@ -0,0 +1,17 @@
+package com.cg.censusanalyser;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class StateAnalyserCensusTest {
+    public static final String STATE_CENSUS_DATA_FILE = "F:\\4. MANMEET JHA\\JAVA\\JAVA_UC_PROBLEMS_INTELLIJ\\Census-Analyser"
+            + "\\IndiaStateCensusData.csv";
+
+    @Test
+    public void testSomeLibraryMethod() {
+        StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
+        int entries = stateCensusAnalyser.loadCsvData(STATE_CENSUS_DATA_FILE);
+        Assert.assertEquals(29, entries);
+    }
+}
