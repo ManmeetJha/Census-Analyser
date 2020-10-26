ca44b4f9c51b4c176cf505c8845283b81a242310
Navneet <navneetjh5@gmail.com>
2020-10-26 22:19:05 +0530

    ADDED: All test cases of StateCode similar to census

--- src/main/java/com/cg/censusanalyser/CustomCensusAnalyserException.java
+++ src/main/java/com/cg/censusanalyser/CustomCensusAnalyserException.java
@@ -2,7 +2,7 @@ package com.cg.censusanalyser;
 
 public class CustomCensusAnalyserException extends Exception {
     enum ExceptionType {
-        IncorrectCsvFile, IncorrectData;
+        IncorrectCsvFile, IncorrectData, NO_DATA;
     }
     ExceptionType type;
     public CustomCensusAnalyserException(String message, ExceptionType type) {
